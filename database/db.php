d24af62c03abb18dfa1151ce322564630c94385e
CurseStaff <clementrse@gmail.com>
2025-01-07 14:08:00 +0100

    implement env protection

--- database/db.php
+++ database/db.php
@@ -1,17 +1,19 @@
 <?php
+require_once __DIR__. '/../security/env.php';
+loadEnv(__DIR__ . '/../.env');
 
-function connectDB(){
-  $host = '51.254.109.35:3306';
-  $user = 'u6646_J2gDRG2QfM';
-  $password = 'gT!SB2uZ@ND44+DXOwZOI3yA';
-  $database = 's6646_bloc3';
-  $conn = new mysqli($host, $user, $password, $database);
+function connectDB()
+{
+    $host = $_ENV['DB_HOST'] ?? '';
+    $user = $_ENV['DB_USER'] ?? '';
+    $password = $_ENV['DB_PASSWORD'] ?? '';
+    $database = $_ENV['DB_NAME'] ?? '';
 
-  if ($conn->connect_error) {
-    die("La connexion à la base de données a échoué : " . $conn->connect_error);
-  }
-  else{
-    return $conn;
-  }
+    $conn = new mysqli($host, $user, $password, $database);
+
+    if ($conn->connect_error) {
+        die("La connexion à la base de données a échoué : " . $conn->connect_error);
+    } else {
+        return $conn;
+    }
 }
-
